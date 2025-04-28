e61cdf5c01dfd30a20573f8c0672a2546fa6654d
Krish Pandey <krishpandey2004@gmail.com>
2025-04-28 20:42:39 +0530

    minor 2

--- coding-duel-backend/routes/friend.js
+++ coding-duel-backend/routes/friend.js
@@ -26,6 +26,21 @@ router.post("/send-friend-request", async (req,res)=>{
       "SELECT user1_id,user2_id,status FROM friends WHERE (user1_id =$1 OR user2_id = $1) AND (user1_id =$2 OR user2_id = $2)",[toUserName,fromUserName]
     );
     console.log(isFriend);
+    if(isFriend.rows.length>0){
+      
+    }
+    else{
+      const status = "pending"
+      const friendReq = await pool.query(
+        "INSERT INTO friends (user1_id, user2_id, status) VALUES ($1, $2, $3) ",
+        [toUserName, fromUserName, status]
+      );
+      res.status(201).json({
+        message: "Freind req sent",
+        
+      });
+    }
+
   }
   catch (err){
     console.log(err);
