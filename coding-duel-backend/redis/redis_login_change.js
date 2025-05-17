fbf75722d9258de6f9b8cdc57177cd324c237c69
Krish Pandey <krishpandey2004@gmail.com>
2025-05-17 17:35:29 +0530

    new login minor 1

--- coding-duel-backend/redis/redis_login_change.js
+++ coding-duel-backend/redis/redis_login_change.js
@@ -1,8 +1,10 @@
+import { getSystemErrorMap } from "util";
 import { redis } from "../redisClient.js";
 
 const change_user = ({old_User,new_User})=>{
     const curSoicketId= redis.get(`socket:${old_User}`);
     redis.del(`socket:${old_User}`);
+    console.log(old_User);
     redis.set(`socket:${new_User}`,curSoicketId);
 } 
 
