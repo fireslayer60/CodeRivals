475e0bf2a6b01ade6d71600d171b07143e652801
Krish Pandey <krishpandey2004@gmail.com>
2025-05-20 18:54:42 +0530

    elo questions minor 6

--- coding-duel-backend/utilities/avg_elo.js
+++ coding-duel-backend/utilities/avg_elo.js
@@ -14,6 +14,7 @@ const avg_elo = async (user1_id,user2_id)=>{
     const user2_elo = user2_res.rows[0].elo;
 
     const avgElo = Math.round((user1_elo + user2_elo) / 2);
+    console.log(avgElo);
 
     return { avgElo };
   } catch (err) {
