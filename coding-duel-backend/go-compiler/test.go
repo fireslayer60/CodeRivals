cfa7094db97b4b7efab211c4c1d2b3170b49baf8
Krish Pandey <krishpandey2004@gmail.com>
2025-06-25 22:32:56 +0530

    minor test 8

--- coding-duel-backend/go-compiler/test.go
+++ coding-duel-backend/go-compiler/test.go
@@ -5,23 +5,18 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/redis/go-redis/v9"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-	fmt.Println(">>> Creating Redis client (v9)...")
-
-	rdb := redis.NewClient(&redis.Options{
-		Addr:         "127.0.0.1:6379", // Ensure no DNS/IPv6 issues
-		DialTimeout:  2 * time.Second,  // Protect against hanging
-		ReadTimeout:  2 * time.Second,
-		WriteTimeout: 2 * time.Second,
-	})
-
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	fmt.Println(">>> Pinging Redis...")
+	fmt.Println(">>> Connecting with go-redis v8...")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+	})
 
 	pong, err := rdb.Ping(ctx).Result()
 	if err != nil {
